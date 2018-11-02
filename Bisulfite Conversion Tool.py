62ca3f586eaa2d3155e0d578c0c803281266f1c4
Haider M. Hassan <44681374+Haider1987@users.noreply.github.com>
2018-11-02 15:03:46 -0400

    Update Bisulfite Conversion Tool.py

--- Bisulfite Conversion Tool.py
+++ Bisulfite Conversion Tool.py
@@ -3,12 +3,6 @@ from tkinter import *
 my_window=Tk()
 my_window.title("Bisulfite Conversion Tool")
 
-#field_value="Field value to output"
-#def copy_text_to_clipboard(event):
-     #field_value=event.widget.get("1.0", "end-1c")
-     #window.clipboard_clear()
-     #window.clipboard_append(field_value)
-
 def convert():
     seq=entry1.get()
     seq2 = seq.upper()
@@ -30,35 +24,20 @@ def convert():
             conv_seq = conv_seq + cur_base
     string_to_display=conv_seq
     second_string_to_display=str(cpg_count)
-    #entry2.grid(row=2,column=2)
     label_2= Label (my_window,text="The bisulfite converted DNA is: ",bg="white",wraplength=550)
     label_2.grid(row=3,column=0)
-    #label_3= Label(my_window, text=string_to_display, bg="lightblue")
-    #label_3.grid(row=3, column=1)
-    #label_3.configure(state="readonly")
-    #text_field=Text(my_window,text=string_to_display)
-    #text_field.insert(1.0,field_value)
-    #text_field.grid(row=3,column=1)
     w=Text(my_window,height=5)
     w.insert(1.0,string_to_display)
-    #w.configure(state="readonly")
     w.grid(row=3,column=1)
     label_3= Label (my_window,text="Number of CpG's in the sequence: ",bg="white",wraplength=550)
     label_3.grid(row=4,column=0)
     y=Text(my_window,height=1)
     y.insert(1.0,second_string_to_display)
     y.grid(row=4,column=1)
-
 label_1=Label(my_window,text="Unconverted Sequence:", bg="white")
-
 entry1=Entry(my_window,width=50,text="hello")
-
 button_1=Button(my_window, text="Bisulfite Convert Please!",bg="lightblue",command=convert)
-
 label_1.grid(row=0, column=0)
 entry1.grid(row=0, column=1)
 button_1.grid(row=2, column=1)
-
-#text_field.bind("<Button-1>",copy_text_to_clipboard)
-
 my_window.mainloop()
